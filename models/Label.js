d0d0dedbebdbe71a1ea490c6cedfe35637d6bee2
Iain Collins <me@iaincollins.com>
2015-05-05 14:21:11 +0100

    Removing cruft from Label model

--- models/Label.js
+++ models/Label.js
@@ -4,11 +4,6 @@ var schema = new mongoose.Schema({
   name: { type: String, unique: true, required: true},
   color: { type: String, required: true},
   order: { type: Number, default: 0 }
-})
-
-schema.pre('save', function(next) {
-  this.path = encodeURI(this.name.toLowerCase().replace(/\//, ''));
-  next();
 });
 
 module.exports = mongoose.model('Label', schema);
